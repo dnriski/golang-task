3742360dc19d85cd31915fce5ed9fc0df78fd43a
dnriski <nugrohoriski159@gmail.com>
2024-06-29 22:48:03 +0700

    CRUD unary delete

--- grpc/controllers/cities.go
+++ grpc/controllers/cities.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"database/sql"
+	"log"
 	"unsia/models"
 	"unsia/pb/cities"
 
@@ -12,13 +13,15 @@ import (
 
 // City struct
 type City struct {
-	DB *sql.DB
+	DB  *sql.DB
+	Log *log.Logger
 	cities.UnimplementedCitiesServiceServer
 }
 
 // GetCity function
 func (s *City) GetCity(ctx context.Context, in *cities.Id) (*cities.City, error) {
 	var cityModel models.City
+	cityModel.Log = s.Log
 	err := cityModel.Get(ctx, s.DB, in)
 	return &cityModel.Pb, err
 
@@ -53,4 +56,13 @@ func (s *City) Create(ctx context.Context, in *cities.CityInput) (*cities.City, 
 	var cityModel models.City
 	err := cityModel.Create(ctx, s.DB, in)
 	return &cityModel.Pb, err
+}
+
+func (s *City) Delete(ctx context.Context, in *cities.Id) (*cities.MyBoolean, error) {
+	var cityModel models.City
+	err := cityModel.Delete(ctx, s.DB, in)
+	if err != nil {
+		return &cities.MyBoolean{Boolean: false}, err
+	}
+	return &cities.MyBoolean{Boolean: true}, nil
 }
--- grpc/models/city.go
+++ grpc/models/city.go
@@ -3,17 +3,21 @@ package models
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"log"
 	"unsia/pb/cities"
 )
 
 type City struct {
-	Pb cities.City
+	Pb  cities.City
+	Log *log.Logger
 }
 
 func (u *City) Get(ctx context.Context, db *sql.DB, in *cities.Id) error {
-	query := `SELECT id, name FROM cities where id = $1`
+	query := `SELECT ids, name FROM cities where id = $1`
 	err := db.QueryRowContext(ctx, query, in.Id).Scan(&u.Pb.Id, &u.Pb.Name)
 	if err != nil {
+		u.Log.Println("Error on Query : ", err)
 		return err
 	}
 	return nil
@@ -35,5 +39,27 @@ func (u *City) Create(ctx context.Context, db *sql.DB, in *cities.CityInput) err
 	u.Pb.Name = in.Name
 	// fmt.Println(rs)
 
+	return nil
+}
+
+func (u *City) Delete(ctx context.Context, db *sql.DB, in *cities.Id) error {
+	query := `DELETE FROM cities WHERE id = $1;`
+	stat, err := db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+
+	rs, err := stat.ExecContext(ctx, in.Id)
+	if err != nil {
+		return err
+	}
+
+	affected, err := rs.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if affected == 0 {
+		return fmt.Errorf("data not found")
+	}
 	return nil
 }
